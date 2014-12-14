05908ca77afc24a761e7a1e8a58f72546bccc3c1
Varun Sood <shanu@Varuns-MacBook-Pro.local>
2014-12-14 14:59:20 -0800

    login 2nd part

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,11 +1,38 @@
 'use strict';
 
 angular.module('arsiaApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl',['$scope','$http', 'auth', function ($scope, $http, auth) {
+    $scope.user={
+      name:"",
+      password:"",
+      email:""
+    };
+
+    $scope.loggedIn=false;
+    $scope.signUp=false;
+
+    $scope.login = function(){
+      if(auth.loginUser($scope.user.email,$scope.user.password)) {
+        $scope.loggedIn=true;
+      }
+    };
+
+    $scope.logout = function(){
+      if(auth.logoutUser()) {
+        $scope.loggedIn=false;
+        $scope.user={};
+      }
+    };
+
+    $scope.signUp = function(){
+      $scope.signUp = true;
+    }
+
+    // Yo stuff
     $scope.awesomeThings = [];
 
     $http.get('/api/things').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
     });
 
-  });
+  }]);
